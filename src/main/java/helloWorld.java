e8a28bf5221682eda0e50b55a0b767f66c1c7cd5
cmitche09 <cmitche09@gmail.com>
2020-09-01 16:52:01 -0400

    First Commit

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,10 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int price = 20;
+        int tShirts = 2;
+        int total = price * tShirts;
+        System.out.println(total);
+    }
+}
